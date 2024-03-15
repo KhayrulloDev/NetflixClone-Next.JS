81c9fe91910ca531fabb740f82dc985d1e9ba2bc
khayrullodev <khayrullodev@khayrullodev.khayrullodev>
2024-03-15 15:16:30 +0500

    globalcontext added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+
 import React from 'react';
 import {Button} from "@/components/ui/button";
+import {useGlobalContext} from "@/context";
 
 const Page = () => {
+    const {account} = useGlobalContext()
+
+    console.log(account)
+
     return (
         <div>
             <Button>Click</Button>
